1007144659add88725d8684e25b25c2267a2142e
황지윤 <hjy648012@naver.com>
2025-12-17 15:21:16 +0900

    feat(wooteco-calculator): Controller 클래스 추가
    - 전체 흐름을 담당할 run 메서드 추가
    - InputView 흐름 추가

---
+++ src/main/java/wooteco/calculator/controller/Controller.java
@@ -0,0 +1,10 @@
+package wooteco.calculator.controller;
+
+import wooteco.calculator.view.InputView;
+
+public class Controller {
+    public void run() {
+        String input = InputView.readInput();
+
+    }
+}
