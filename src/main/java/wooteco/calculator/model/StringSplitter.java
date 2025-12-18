b2ab132d72b304f8c4d816069943739d6c85fc83
황지윤 <hjy648012@naver.com>
2025-12-18 19:51:32 +0900

    feat(wooteco-calculator): StringSplitter 클래스 구현
    - 기본 구분자만 있는 경우와 커스텀 구분자까지 있는 경우를 분리
    - 구분자를 기준으로 입력 받은 문자열을 분리

--- src/main/java/wooteco/calculator/model/StringSplitter.java
+++ src/main/java/wooteco/calculator/model/StringSplitter.java
@@ -1,4 +1,26 @@
 package wooteco.calculator.model;
 
-public class Delimiter {
+public class StringSplitter {
+    private String[] splitedInput;
+
+    public StringSplitter(String input) {
+        this.splitedInput = splitString(input);
+    }
+
+    private String[] splitString(String input) {
+        DelimiterFinder delimiterFinder = new DelimiterFinder(input);
+        String customDelimiter = delimiterFinder.getCustomDelimiter();
+
+        if (!input.contains("\n")) {
+            this.splitedInput = input.split(", | :");
+        } else {
+            this.splitedInput = input.split(",|:|" + customDelimiter);
+        }
+
+        return splitedInput;
+    }
+
+    public String[] getSplitedInput() {
+        return splitedInput;
+    }
 }
