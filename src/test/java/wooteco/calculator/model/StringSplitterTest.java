5813ceca41a692c1c03ce6afdf8efb17e929f7bf
황지윤 <hjy648012@naver.com>
2025-12-19 13:29:20 +0900

    test(wooteco-calculator): StringSplitterTest 추가
    - 문자열을 구분자로 분리하는 테스트

---
+++ src/test/java/wooteco/calculator/model/StringSplitterTest.java
@@ -0,0 +1,19 @@
+package wooteco.calculator.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringSplitterTest {
+    @DisplayName("문자열을 구분자로 분리한다")
+    @Test
+    void splitString_문자열을_구분자로_분리한다() {
+        String input = "//;\n1;2;3";
+        StringSplitter stringSplitter = new StringSplitter(input);
+
+        String[] result = stringSplitter.getSplitedInput();
+
+        assertThat(result).containsExactly("1", "2", "3");
+    }
+}
