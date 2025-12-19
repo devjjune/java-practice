3b0a8219db2e4589accdfe695e4025df2fffa214
황지윤 <hjy648012@naver.com>
2025-12-19 18:44:58 +0900

    feat(calc): Service 클래스 추가
    - DelimiterFinder, StringSplitter 로직 흐름 추가

--- src/main/java/wooteco/calculator/service/Service.java
+++ src/main/java/wooteco/calculator/service/Service.java
@@ -1,2 +1,14 @@
-package wooteco.calculator.service;public class Service {
+package wooteco.calculator.service;
+
+import wooteco.calculator.model.DelimiterFinder;
+import wooteco.calculator.model.StringSplitter;
+
+public class Service {
+    private void parseInput(String input) {
+        DelimiterFinder delimiterFinder = new DelimiterFinder(input);
+        String customDelimiter = delimiterFinder.getCustomDelimiter();
+
+        StringSplitter stringSplitter = new StringSplitter(input, customDelimiter);
+        String[] splitedInput = stringSplitter.getSplitedInput();
+    }
 }
