1d030dafa5aeb67a8fbfb5a4ab3d22bc509b7c9f
황지윤 <hjy648012@naver.com>
2025-12-20 19:15:18 +0900

    test(calc): NumberParserTest 추가

--- src/test/java/wooteco/calculator/model/NumberParserTest.java
+++ src/test/java/wooteco/calculator/model/NumberParserTest.java
@@ -1,2 +1,21 @@
-package wooteco.calculator.model;public class NumberParserTest {
+package wooteco.calculator.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class NumberParserTest {
+    @DisplayName("문자열을 숫자로 바꾼다")
+    @Test
+    void parse_문자열을_숫자로_바꾼다() {
+        String[] splitedInput = {"1", "2", "3"};
+        NumberParser numberParser = new NumberParser(splitedInput);
+
+        List<Integer> result = numberParser.getNumbers();
+
+        assertThat(result).containsExactly(1, 2, 3);
+    }
 }
