76e88f1e0751e9e0c9afbe89359d30f3972ec4e9
황지윤 <hjy648012@naver.com>
2025-12-21 15:07:55 +0900

    feat(calc): Service 클래스 수정
    - NumberParser, Calculator 흐름 추가

--- src/main/java/wooteco/calculator/service/Service.java
+++ src/main/java/wooteco/calculator/service/Service.java
@@ -1,14 +1,25 @@
 package wooteco.calculator.service;
 
+import wooteco.calculator.model.Calculator;
 import wooteco.calculator.model.DelimiterFinder;
+import wooteco.calculator.model.NumberParser;
 import wooteco.calculator.model.StringSplitter;
 
+import java.util.List;
+
 public class Service {
-    private void parseInput(String input) {
+    public int parseInput(String input) {
         DelimiterFinder delimiterFinder = new DelimiterFinder(input);
         String customDelimiter = delimiterFinder.getCustomDelimiter();
 
         StringSplitter stringSplitter = new StringSplitter(input, customDelimiter);
         String[] splitedInput = stringSplitter.getSplitedInput();
+
+        NumberParser numberParser = new NumberParser();
+        List<Integer> numbers = numberParser.parse(splitedInput);
+
+        Calculator calculator = new Calculator();
+        return calculator.sum(numbers);
+
     }
 }
