3d14b31424b02e3dcb37ad5473b7ac1a7875bef9
황지윤 <hjy648012@naver.com>
2025-12-21 15:27:41 +0900

    feat(calc): main 메서드에서 controller 호출

--- src/main/java/wooteco/calculator/Application.java
+++ src/main/java/wooteco/calculator/Application.java
@@ -1,7 +1,10 @@
 package wooteco.calculator;
 
+import wooteco.calculator.controller.Controller;
+
 public class Application {
     public static void main(String[] args) {
-
+        Controller controller = new Controller();
+        controller.run();
     }
 }
