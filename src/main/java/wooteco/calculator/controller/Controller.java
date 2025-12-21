46556a5a980952e6977180917d96da75742f5945
황지윤 <hjy648012@naver.com>
2025-12-21 15:13:10 +0900

    feat(calc): Controller 수정
    - Service 흐름 추가

--- src/main/java/wooteco/calculator/controller/Controller.java
+++ src/main/java/wooteco/calculator/controller/Controller.java
@@ -1,10 +1,14 @@
 package wooteco.calculator.controller;
 
+import wooteco.calculator.service.Service;
 import wooteco.calculator.view.InputView;
 
 public class Controller {
     public void run() {
         String input = InputView.readInput();
+
+        Service service = new Service();
+        int result = service.parseInput(input);
 
     }
 }
