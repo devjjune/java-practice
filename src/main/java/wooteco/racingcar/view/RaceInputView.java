ad959e98dd6e1a99f464400490c9ba0d992be22e
황지윤 <hjy648012@naver.com>
2025-12-23 20:26:30 +0900

    refactor(race): 입력 안내 메시지를 final 변수로 선언

--- src/main/java/wooteco/racingcar/view/RaceInputView.java
+++ src/main/java/wooteco/racingcar/view/RaceInputView.java
@@ -5,10 +5,12 @@ import java.util.List;
 import java.util.Scanner;
 
 public class RaceInputView {
+    final String NAMES_MESSSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    final String TRIES_MESSAGE = "시도할 횟수는 몇 회인가요?";
     Scanner scanner = new Scanner(System.in);
 
     public List<String> readNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(NAMES_MESSSAGE);
         String inputName = scanner.nextLine();
         String[] carArray = inputName.split(",");
 
@@ -16,7 +18,7 @@ public class RaceInputView {
     }
 
     public int readTries() {
-        System.out.println("시도할 횟수는 몇 회인가요?");
+        System.out.println(TRIES_MESSAGE);
         String inputTries = scanner.nextLine();
 
         return Integer.parseInt(inputTries);
