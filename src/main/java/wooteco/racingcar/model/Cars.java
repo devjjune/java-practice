e409c6f7658a75cc3a32cadf2ee7c4ad3c0215e7
황지윤 <hjy648012@naver.com>
2025-12-30 21:55:01 +0900

    refactor(race): 코드 구조 개선
    - InputView 로직에서 Scanner 객체 생성 중복 제거
    - getCars() 메서드에 불변 리스트(Collections.unmodifiableList) 적용

--- src/main/java/wooteco/racingcar/model/Cars.java
+++ src/main/java/wooteco/racingcar/model/Cars.java
@@ -47,6 +47,6 @@ public class Cars {
     }
 
     public List<Car> getCars() {
-        return cars;
+        return Collections.unmodifiableList(cars);
     }
 }
--- src/main/java/wooteco/racingcar/view/RaceInputView.java
+++ src/main/java/wooteco/racingcar/view/RaceInputView.java
@@ -3,19 +3,18 @@ package wooteco.racingcar.view;
 import java.util.Scanner;
 
 public class RaceInputView {
-    final static String NAMES_MESSSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    final static String TRIES_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private static final String NAMES_MESSSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String TRIES_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static String readNames() {
-        Scanner scanner = new Scanner(System.in);
         System.out.println(NAMES_MESSSAGE);
         return scanner.nextLine();
     }
 
     public static int readTries() {
-        Scanner scanner = new Scanner(System.in);
         System.out.println(TRIES_MESSAGE);
-
-        return Integer.parseInt(scanner.nextLine());
+        String input = scanner.nextLine();
+        return Integer.parseInt(input);
     }
 }
